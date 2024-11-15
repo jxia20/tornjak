80bd077d4422e069e6215eef70d9bec9bc9d168b
DashLHall <153118685+DashLHall@users.noreply.github.com>
2024-11-15 16:17:05 -0500

    debugging spire health-check file removed to much originally re-implemented some

--- frontend/src/components/spire-health-check.tsx
+++ frontend/src/components/spire-health-check.tsx
@@ -1,29 +1,20 @@
-import { Component } from 'react';
 import './style.css';
 import { RootState } from 'redux/reducers';
 import { connect } from 'react-redux';
-import { InlineLoading } from 'carbon-components-react';
+import { Tooltip, InlineLoading } from 'carbon-components-react';
 import TornjakApi from './tornjak-api-helpers';
-import {
-  spireHealthCheckFunc,
-  spireHealthCheckingFunc,
-} from 'redux/actions';
+import { spireHealthCheckFunc, spireHealthCheckingFunc } from 'redux/actions';
 
 type SpireHealthCheckProp = {
-  // dispatches a payload for the status of SPIRE health as a boolean has a return type of void
   spireHealthCheckFunc: (globalSpireHealthCheck: boolean) => void,
-  // the status of SPIRE health
   globalSpireHealthCheck: boolean,
-  // dispatches a payload for the loading state of SPIRE health as a boolean and has a return type of void
   spireHealthCheckingFunc: (globalSpireHealthCheck: boolean) => void,
-  // the loading state of SPIRE health
   globalSpireHealthChecking: boolean,
-}
+};
 
 type SpireHealthCheckState = {
-  // timer to check SPIRE health
-  timer: NodeJS.Timeout | Date | null,
-}
+  timer: NodeJS.Timeout | null,
+};
 
 class SpireHealthCheck extends Component<SpireHealthCheckProp, SpireHealthCheckState> {
   TornjakApi: TornjakApi;
@@ -42,7 +33,6 @@ class SpireHealthCheck extends Component<SpireHealthCheckProp, SpireHealthCheckS
 
   componentDidUpdate(prevProps: SpireHealthCheckProp, prevState: SpireHealthCheckState) {
     if (prevState.timer !== this.state.timer) {
-      // timer ended, check status of SPIRE
       this.TornjakApi.spireHealthCheck(this.props.spireHealthCheckFunc, this.props.spireHealthCheckingFunc);
     }
   }
@@ -50,33 +40,37 @@ class SpireHealthCheck extends Component<SpireHealthCheckProp, SpireHealthCheckS
   startTimer = () => {
     const timer = setTimeout(() => {
       this.setState({ timer: new Date() });
-      this.startTimer(); // restart timer
-    }, 60000); // default time to check SPIRE health in milliseconds
+      this.startTimer(); // Restart timer
+    }, 60000); // Default refresh rate: 1 minute
     this.setState({ timer });
   };
 
   render() {
-    const success = this.props.globalSpireHealthCheck, checking = this.props.globalSpireHealthChecking;
-    let spireStatus =
+    const { globalSpireHealthCheck, globalSpireHealthChecking } = this.props;
+    let spireStatus = (
       <div>
-        <InlineLoading
-          status={success ? 'finished' : 'error'}
-          description={success ? 'Active' : 'InActive'} />
+        {globalSpireHealthChecking ? (
+          <InlineLoading description="Checking SPIRE health..." />
+        ) : globalSpireHealthCheck ? (
+          <p>SPIRE is healthy</p>
+        ) : (
+          <p>SPIRE is unhealthy</p>
+        )}
       </div>
+    );
+
     return (
       <div className="health-check">
-        <div className='health-status-check-container'>
-          <div className='health-status-check-title'>
+        <div className="spire-health-refresh-tooltip">
+          <Tooltip>
+            <p className="spire-health-helper">SPIRE Health Check Status</p>
+          </Tooltip>
+        </div>
+        <div className="health-status-check-container">
+          <div className="health-status-check-title">
             <h6>SPIRE: </h6>
           </div>
-          <div className='health-status-check-icon'>
-            {!checking &&
-              <InlineLoading
-                status="active"
-                description="Checking" />
-            }
-            {checking && spireStatus}
-          </div>
+          {spireStatus}
         </div>
       </div>
     );
@@ -86,11 +80,8 @@ class SpireHealthCheck extends Component<SpireHealthCheckProp, SpireHealthCheckS
 const mapStateToProps = (state: RootState) => ({
   globalSpireHealthCheck: state.servers.globalSpireHealthCheck,
   globalSpireHealthChecking: state.servers.globalSpireHealthChecking,
-})
+});
 
-export default connect(
-  mapStateToProps,
-  { spireHealthCheckFunc, spireHealthCheckingFunc }
-)(SpireHealthCheck)
+export default connect(mapStateToProps, { spireHealthCheckFunc, spireHealthCheckingFunc })(SpireHealthCheck);
+export { SpireHealthCheck };
 
-export { SpireHealthCheck }
