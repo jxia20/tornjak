cc833306f6cb28a7cc3f09327fea2d87cb1eee2f
DashLHall <153118685+DashLHall@users.noreply.github.com>
2024-12-10 20:03:26 -0500

    debug on table header

--- frontend/src/tables/table-head.tsx
+++ frontend/src/tables/table-head.tsx
@@ -1,50 +1,51 @@
 import React from "react";
-import { DataTable, DataTableCustomHeaderData, DataTableCustomHeaderProps, DataTableCustomSelectionData, DataTableCustomSelectionProps, DataTableHeader, DataTableRow } from "carbon-components-react";
+import {
+  DataTable,
+  DataTableCustomHeaderData,
+  DataTableCustomHeaderProps,
+  DataTableCustomSelectionData,
+  DataTableCustomSelectionProps,
+  DataTableHeader,
+  DataTableRow,
+} from "carbon-components-react";
 import { ReactAttr, ShapeOf } from "carbon-components-react/typings/shared";
-const {
-    TableHead,
-    TableRow,
-    TableSelectAll,
-    TableHeader,
-} = DataTable;
 
-// Head takes in 
-// getSelectionProps: getSelectionProps function for selecting all rows from DataTable
-// headers: headerData of table
-// getHeaderProps: getHeaderProps function from DataTable
-// returns header of the table for the specified entity
+const { TableHead, TableRow, TableSelectAll, TableHeader } = DataTable;
+
+// Head takes in:
+// - getSelectionProps: Function for selecting all rows from DataTable
+// - headers: Header data of the table
+// - getHeaderProps: Function to get header properties from DataTable
+// Returns the header of the table for the specified entity
 type HeadProp = {
-    headers: DataTableHeader<string>[],
-    getSelectionProps: <E extends object = {}>(data?: 
-                        ShapeOf<DataTableCustomSelectionData<DataTableRow<string>>, E> | undefined) => 
-                        ShapeOf<DataTableCustomSelectionProps<DataTableRow<string>>, E> | 
-                        ShapeOf<DataTableCustomSelectionProps<never>, E>,
-    getHeaderProps: <E extends object = 
-                    ReactAttr<HTMLElement>>(data: ShapeOf<DataTableCustomHeaderData<DataTableHeader<string>>, E>) => 
-                    ShapeOf<DataTableCustomHeaderProps<DataTableHeader<string>>, E>,
-}
-
-type HeadState = {}
-class Head extends React.Component<HeadProp, HeadState> {
-    constructor(props: HeadProp) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <TableHead>
-                <TableRow>
-                    <TableSelectAll {...this.props.getSelectionProps()} />
-                    {this.props.headers.map((header: DataTableHeader<string>) => (
-                        <TableHeader {...this.props.getHeaderProps({ header })}>
-                            {header.header}
-                        </TableHeader>
-                    ))}
-                </TableRow>
-            </TableHead>
-        )
-    }
+  headers: DataTableHeader<string>[];
+  getSelectionProps: <E extends object = {}>(
+    data?:
+      | ShapeOf<DataTableCustomSelectionData<DataTableRow<string>>, E>
+      | undefined
+  ) => ShapeOf<DataTableCustomSelectionProps<DataTableRow<string>>, E> | ShapeOf<DataTableCustomSelectionProps<never>, E>;
+  getHeaderProps: <E extends object = ReactAttr<HTMLElement>>(
+    data: ShapeOf<DataTableCustomHeaderData<DataTableHeader<string>>, E>
+  ) => ShapeOf<DataTableCustomHeaderProps<DataTableHeader<string>>, E>;
 };
 
-export default (Head)
+class Head extends React.Component<HeadProp> {
+  render() {
+    const { headers, getSelectionProps, getHeaderProps } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableSelectAll {...getSelectionProps()} />
+          {headers.map((header) => (
+            <TableHeader key={header.key} {...getHeaderProps({ header })}>
+              {header.header}
+            </TableHeader>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+export default Head;
